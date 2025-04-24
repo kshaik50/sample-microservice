bb669cf4a46fd676966a62ebac09cc4241fa1c5a
kshaik50 <143249893+kshaik50@users.noreply.github.com>
2025-04-23 19:17:01 -0500

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav style={{ padding: '1rem', backgroundColor: '#282c34', color: '#fff' }}>
+    <h3>React + Jenkins + K8s</h3>
+  </nav>
+);
+
+export default Navbar;
