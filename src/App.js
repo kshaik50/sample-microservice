7045691cc65a1fa693e3be9274742abc3369c252
kshaik50 <143249893+kshaik50@users.noreply.github.com>
2025-04-23 19:16:27 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1 style={{ textAlign: 'center' }}>🚀 Welcome to React CI/CD with Jenkins + K8s!</h1>
+    </div>
+  );
+}
+
+export default App;
